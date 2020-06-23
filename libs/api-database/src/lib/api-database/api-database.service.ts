0a3d659f4c98d6aa32bffefd877f591cbbfd0478
Carlos Casa <duardbr@gmail.com>
2020-06-23 11:53:14 -0300

    Carlos - api-database [raw-sql, table-fields]

--- libs/api-database/src/lib/api-database/api-database.service.ts
+++ libs/api-database/src/lib/api-database/api-database.service.ts
@@ -3,13 +3,18 @@ import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class ApiDatabaseService {
-
-  constructor(private readonly entityManager: EntityManager) { }
+  constructor(private readonly entityManager: EntityManager) {}
 
   async rawSQL(sqlQuery: any) {
-    const rawData = await this.entityManager.query(`SHOW FULL TABLES`);
+    const rawData = await this.entityManager.query(sqlQuery);
+    return rawData;
+  }
+
+  tableFields(tableName: string) {
+    const rawData = this.entityManager.query(`SHOW COLUMNS FROM ${tableName}`);
     return rawData;
   }
 }
 
 // const rawData = await manager.query(`SELECT * FROM USERS`);
+// `SHOW COLUMNS FROM City`
