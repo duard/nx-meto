b3b7f6b42353a6998d8c113c6c51827980e8b925
Carlos Casa <duardbr@gmail.com>
2020-07-25 11:48:10 -0300

    [menu] Build no Surge - LISTA;

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit, OnDestroy {
     private _translateService: TranslateService,
     private _platform: Platform
   ) {
-    console.log('Versão busca menu', 'Build');
+    console.log('Versão busca menu', 'Build', 'outro teste');
     // Get default navigation
     this.navigation = navigation;
 
